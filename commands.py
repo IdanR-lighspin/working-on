a385185d3fc1f2e95e92716e34d39d87f0842ff8
IdanR-lighspin <90135915+IdanR-lighspin@users.noreply.github.com>
2021-10-03 12:31:35 +0300

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,120 @@
+import subprocess
+import time
+start_time = time.time()
+command9 = "cd /home/ubuntu/vuls"
+try:
+    command1 = "sudo apt-get update -y"
+    output = subprocess.getoutput(command1)  # saving terminal's output
+except Exception as e:
+    print(e)
+
+# chkrootkit:
+try:
+    command2 = "sudo apt-get install chkrootkit -y"
+    output = subprocess.getoutput(command2)
+    print("downloaded chkrootkit.")
+    # activate by running "sudo chkrootkit" in terminal.
+except Exception as e:
+    print("problem with downloading chkrootkit:")
+    print(e)
+
+# lynis:
+try:
+    command3 = "sudo apt-get install apt-transport-https -y"
+    command4 = "sudo apt-get install lynis -y"
+    output = subprocess.getoutput(command3)
+    output = subprocess.getoutput(command4)
+    # activate by running "sudo lynis audit system" in terminal
+    print("downloaded lynis.")
+except Exception as e:
+    print("problem with downloading lynis:")
+    print(e)
+
+
+# vuls :(
+
+# keep in mind the ssh keygen thing.
+try:
+    command5 = "sudo apt install docker.io -y"
+    command6 = "sudo apt install docker -y"
+    output = subprocess.getoutput(command5+"; "+command6)
+    print("downloaded docker.")
+except Exception as e:
+    print("problem with downloading docker:")
+    print(e)
+try:
+    command7 = "sudo docker pull vuls/go-cve-dictionary; " \
+               "sudo docker pull vuls/goval-dictionary; " \
+               "sudo docker pull vuls/gost; " \
+               "sudo docker pull vuls/go-exploitdb;" \
+               "sudo docker pull vuls/gost; " \
+               "sudo docker pull vuls/vuls; "
+    output = subprocess.getoutput(command7)
+    print("pulled first vuls thing.")
+except Exception as e:
+    print("problem with pulling vuls things")
+    print(e)
+try:
+    command8 = "mkdir /home/ubuntu/vuls"
+    command10 = "mkdir go-cve-dictionary-log goval-dictionary-log gost-log go-exploitdb-log go-msfdb-log"
+    output = subprocess.getoutput(command8+"; "+command9+"; "+command10)
+except Exception as e:
+    print("problem with creating vuls dirs:")
+    print(e)
+"""
+try:
+    
+    command9 = "cd /home/ubuntu/vuls"
+    command11 = "sudo docker run --rm -it \
+        -v $PWD:/go-cve-dictionary \
+        -v $PWD/go-cve-dictionary-log:/var/log/go-cve-dictionary \
+        vuls/go-cve-dictionary fetch nvd"
+    command12 = "sudo docker run --rm -it \
+        -v $PWD:/goval-dictionary \
+        -v $PWD/goval-dictionary-log:/var/log/goval-dictionary \
+        vuls/goval-dictionary fetch ubuntu 20"
+    command13 = "sudo docker run --rm -i \
+        -v $PWD:/gost \
+        -v $PWD/gost-log:/var/log/gost \
+        vuls/gost fetch ubuntu"
+    command14 = "sudo docker run --rm -i \
+        -v $PWD:/go-exploitdb \
+        -v $PWD/go-exploitdb-log:/var/log/go-exploitdb \
+        vuls/go-exploitdb fetch exploitdb"
+    command15 = "sudo docker run --rm -i \
+        -v $PWD:/go-msfdb \
+        -v $PWD/go-msfdb-log:/var/log/go-msfdb \
+        vuls/go-msfdb fetch msfdb"
+    output = subprocess.getoutput(command9+"; "+command11+"; "+command12+"; "+command13+"; "+command14+"; "+command15)
+    print("fetched DBs")
+except Exception as e:
+    print("error fetching DBs")
+    print(e)
+"""
+try:
+
+    command16 = "sudo echo | ssh-keygen -P ''"
+    output = subprocess.getoutput(command9 + "; " + command16)  # sudo su?
+    print("generated ssh key")
+except Exception as e:
+    print("problem with creating ssh key:")
+    print(e)
+
+try:
+    # todo: need to work on the ssh thing.
+    command17 = """cat > config.toml <<EOF
+[servers]
+[servers.localhost]
+host = "localhost"
+port = "local"
+EOF
+"""
+    output = subprocess.getoutput(command9+"; "+command17)
+    print("created config file")
+except Exception as e:
+    print(e)
+
+# BEFORE RUNNING NEED TO cd to vuls dir
+
+
+print("took to execute: ", time.time()-start_time)  # about 6 minutes
