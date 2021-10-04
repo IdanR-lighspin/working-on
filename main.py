9e588009def72b47685f7a4090c0867f97183fad
IdanR-lighspin <90135915+IdanR-lighspin@users.noreply.github.com>
2021-10-04 17:05:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def create_ec2(security_group_id, key_pair_name):
 
 
 def instance_id_ip(instance_id):
+    ec2_global_ip = ""
     # instance_id = "i-073ae94f0d3e7b4d3"
     temp = boto.ec2.connect_to_region(region)
     reservations = temp.get_all_instances()
@@ -105,13 +106,13 @@ def instance_id_ip(instance_id):
 
 def ssh_operations(ec2_global_ip, key_pair_name, sudo_pass, file_path_to_pass, destination_file_path):
     """
-    transferring and running python file in the new ec2 machine (or another machine if wanted..)
+    transferring and running a python file in the new ec2 machine (or another machine if wanted..)
     :param ec2_global_ip:
     :param key_pair_name:
     :param sudo_pass:
     :return:
     """
-    # IMPORTANT- without chmod 400 (etc) the ssh will not work due to too open access to the key file
+    # IMPORTANT- without chmod 400 (etc) the ssh will not work due to too open access to the key file.
     sudo_password = "echo " + sudo_pass + " | sudo -S "
     commands1 = sudo_password + ";" + "sudo chmod 400 /"+key_pair_name+".pem"
     output = subprocess.getoutput(commands1)
@@ -134,34 +135,39 @@ def ssh_operations(ec2_global_ip, key_pair_name, sudo_pass, file_path_to_pass, d
         print(line)
 
 
-key_pair_name = "idan8"  # without .pem
-sudo_password = "Idan2408"
+def main():
+    key_pair_name = "idan8"  # without .pem
+    sudo_password = "Idan2408"
+    
+    vpc_id = get_vpc()
+    print(vpc_id)
+    
+    # security_group_id = security_group_settings(vpc_id)
+    # print(security_group_id)
+    
+    # instance_id = create_ec2(security_group_id, key_pair_name)
+    instance_id = "i-073ae94f0d3e7b4d3"
+    
+    global_ip = instance_id_ip(instance_id)
+    
+    a = Creator("us-east-2", 'i-073ae94f0d3e7b4d3',  "i-0e379dc5b2efc913c")  # (region, ec2 just created, ec2 to snap from)
+    a.create_and_attach_volume_from_snapshot()
+    ssh_operations(global_ip, key_pair_name, sudo_password, "/home/idan/PycharmProjects/pythonProject/commands.py", "/home/ubuntu/test.py")
+    ssh_operations(global_ip, key_pair_name, sudo_password, "/home/idan/PycharmProjects/pythonProject/main_elk.py", "/home/ubuntu/test1.py")
+    
+    # IN THIS POINT: we have new ec2 running and all the things installed there.
+    # after running this once, need to change device name (there are list of available names. ill handle it later
+    # need to run just this function:
+    
+    # IN THIS POINT: we attached the new volume to the new ec2, and mounted it the the commands file.
+    
+    print("took to execute: ", time.time()-start_time)  # about 6 minutes
+    
 
-vpc_id = get_vpc()
-print(vpc_id)
-
-# security_group_id = security_group_settings(vpc_id)
-# print(security_group_id)
-
-# instance_id = create_ec2(security_group_id, key_pair_name)
-instance_id = "i-073ae94f0d3e7b4d3"
-
-global_ip = instance_id_ip(instance_id)
-
-a = Creator("us-east-2", 'i-073ae94f0d3e7b4d3',  "i-0e379dc5b2efc913c")  # (region, ec2 just created, ec2 to snap from)
-a.create_and_attach_volume_from_snapshot()
-ssh_operations(global_ip, key_pair_name, sudo_password, "/home/idan/PycharmProjects/pythonProject/commands.py", "/home/ubuntu/test.py")
-ssh_operations(global_ip, key_pair_name, sudo_password, "/home/idan/PycharmProjects/pythonProject/main_elk.py", "/home/ubuntu/test1.py")
-
-# IN THIS POINT: we have new ec2 running and all the things installed there.
-# after running this once, need to change device name (there are list of available names. ill handle it later
-# need to run just this function:
-
-
-# IN THIS POINT: we attached the new volume to the new ec2, and mounted it the the commands file.
-
-print("took to execute: ", time.time()-start_time)  # about 6 minutes
-
+if __name__ == "__main__":
+    main()
+    
 # todo: get device name probably with psutil- not sure that its possible.
 # todo: vuls isn't outputting correctly. need to look on the config file probably.
 # todo: need to run elastic from different machine and port forward the kibana port. now the kibana code is commented.
+
