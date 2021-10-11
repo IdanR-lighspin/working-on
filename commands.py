fcb652d48a552ad47553d93b6afcb874102814cf
IdanR-lighspin <90135915+IdanR-lighspin@users.noreply.github.com>
2021-10-11 12:16:02 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -5,6 +5,35 @@ import os
 
 start_time = time.time()
 command9 = "cd /home/ubuntu/vuls"
+new_vol_name = "newvolume1"
+try:
+    output = subprocess.getoutput("lsblk --json -fs")
+    lsblk = json.loads(output)
+    mount_dev = "/dev/xvdu1"  # prevent the maybe undefined.
+    for item in lsblk["blockdevices"]:
+        if item["fstype"] in ["ext2", "xfs", "ext3", "ext4"] and not (item['mountpoint']):
+            mount_dev = f"/dev/{item['name']}"
+
+
+    disk_name = str(mount_dev)
+    commands = "cd /; sudo mkdir /"+new_vol_name+"/; sudo mount "+disk_name+" /"+new_vol_name+"/"
+    output = subprocess.getoutput(commands)
+    print("mount-ed the new volume")
+    print("mount-ed the new volume")
+except Exception as e:
+    print("failed to mount the new volume:")
+    print(e)
+    # if it crashed: may ran this file before the creator.
+    # means we try to mount a file that didn't even created.
+try:
+    command20 = """sudo mount -t proc none /{fname}/proc; 
+sudo mount -o bind /dev /{fname}/dev; 
+sudo mount -o bind /sys /{fname}/sys; 
+sudo mount -o bind /run /{fname}/run""".format(fname=new_vol_name)
+    output = subprocess.getoutput(command20)
+except Exception as e:
+    print("problem:")
+    print(e)
 try:
     command1 = "sudo apt-get update -y"
     output = subprocess.getoutput(command1)  # saving terminal's output
@@ -21,7 +50,7 @@ except Exception as e:
     print("problem with downloading chkrootkit:")
     print(e)
 
-# lynis:
+# lynis on the entire ec2:
 try:
     command3 = "sudo apt-get install apt-transport-https -y"
     command4 = "sudo apt-get install lynis -y"
@@ -32,8 +61,17 @@ try:
 except Exception as e:
     print("problem with downloading lynis:")
     print(e)
-
-
+# lynis right to the new volume:
+try:
+    command21 = """sudo chroot /{fname} /bin/bash <<"EOT"
+sudo apt-get install --fix-missing
+sudo apt-get install apt-transport-https -y
+sudo apt-get install lynis -y
+echo $$
+EOT""".format(fname=new_vol_name)
+    output = subprocess.getoutput(command21)
+except Exception as e:
+    print("problem with installimg lynis to the new root")
 # vuls :(
 
 # keep in mind the ssh keygen thing.
@@ -117,24 +155,7 @@ EOF
 except Exception as e:
     print(e)
 
-try:
-    output = subprocess.getoutput("lsblk --json -fs")
-    lsblk = json.loads(output)
-    mount_dev = "/dev/xvdu1"  # prevent the maybe undefined.
-    for item in lsblk["blockdevices"]:
-        if item["fstype"] in ["ext2", "xfs", "ext3", "ext4"] and not (item['mountpoint']):
-            mount_dev = f"/dev/{item['name']}"
 
-    disk_name = str(mount_dev)
-    commands = "cd /; sudo mkdir /newvolume1/; sudo mount "+disk_name+" /newvolume1/"
-    output = subprocess.getoutput(commands)
-    print("mount-ed the new volume")
-    print("mount-ed the new volume")
-except Exception as e:
-    print("failed to mount the new volume:")
-    print(e)
-    # if it crashed: may ran this file before the creator.
-    # means we try to mount a file that didn't even created.
 
 
 print("commands took to execute: ", time.time()-start_time)  # about 6 minutes
